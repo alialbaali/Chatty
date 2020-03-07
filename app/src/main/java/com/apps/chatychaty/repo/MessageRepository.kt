20aed100b3e499408b85523b8b056b2522a6f798
alialbaali <alialbaali@outlook.com>
2020-03-07 17:18:24 +0300

    Implement User & Message Repository

--- app/src/main/java/com/apps/chatychaty/repo/MessageRepository.kt
+++ app/src/main/java/com/apps/chatychaty/repo/MessageRepository.kt
@@ -1,6 +1,29 @@
 package com.apps.chatychaty.repo
 
+import com.apps.chatychaty.model.Message
 import com.apps.chatychaty.network.MessageClient
-import com.apps.chatychaty.network.UserClient
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
-class MessageRepository(private val messageClient: MessageClient)
+class MessageRepository(private val messageClient: MessageClient) {
+
+    suspend fun postMessage(message: Message) {
+        withContext(Dispatchers.IO) {
+            messageClient.postMessage(message)
+        }
+    }
+
+    suspend fun getMessages(): List<Message> {
+        return withContext(Dispatchers.IO) {
+            messageClient.getMessages()
+        }
+    }
+
+    suspend fun getImg(){
+        withContext(Dispatchers.IO){
+            messageClient.getImg()
+        }
+        TODO("make the function return an image type")
+    }
+}
+
--- app/src/main/java/com/apps/chatychaty/repo/UserRepository.kt
+++ app/src/main/java/com/apps/chatychaty/repo/UserRepository.kt
@@ -1,5 +1,22 @@
 package com.apps.chatychaty.repo
 
+import com.apps.chatychaty.model.Response
+import com.apps.chatychaty.model.User
 import com.apps.chatychaty.network.UserClient
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
-class UserRepository(private val userClient: UserClient)
+class UserRepository(private val userClient: UserClient) {
+
+    suspend fun createAccount(user: User): Response {
+        return withContext(Dispatchers.IO) {
+            userClient.createAccount(user)
+        }
+    }
+
+    suspend fun logIn(user: User): Response {
+        return withContext(Dispatchers.IO) {
+            userClient.createAccount(user)
+        }
+    }
+}
