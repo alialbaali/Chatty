d43f3f6c28ee93e2dca6d6b497fcfe5f0c75431b
alialbaali <alialbaali@outlook.com>
2020-03-26 16:56:47 +0300

    Implement Profile edit name functionality

--- app/src/main/java/com/apps/chatychaty/ui/ProfileFragment.kt
+++ app/src/main/java/com/apps/chatychaty/ui/ProfileFragment.kt
@@ -1,24 +1,45 @@
 package com.apps.chatychaty.ui
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
+import android.database.Cursor
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
+import androidx.core.content.edit
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.apps.chatychaty.DURATION
 import com.apps.chatychaty.R
 import com.apps.chatychaty.databinding.FragmentProfileBinding
+import com.apps.chatychaty.network.Repos
+import com.apps.chatychaty.viewModel.Error
+import com.apps.chatychaty.viewModel.ProfileViewModel
+import com.apps.chatychaty.viewModel.ProfileViewModelFactory
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.transition.MaterialSharedAxis
+import timber.log.Timber
+import java.io.File
+import java.net.URI
 
 /**
  * A simple [Fragment] subclass.
  */
-class ProfileFragment : Fragment() {
+class ProfileFragment : Fragment(), Error, UpdateName {
 
     private lateinit var binding: FragmentProfileBinding
+
+    private val viewModel by viewModels<ProfileViewModel> {
+        ProfileViewModelFactory(Repos.userRepository)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,7 +47,8 @@ class ProfileFragment : Fragment() {
     ): View? {
         binding = FragmentProfileBinding.inflate(inflater, container, false)
 
-
+        viewModel.error = this
+        viewModel.updateName = this
 
         binding.tb.setNavigationOnClickListener {
 
@@ -40,11 +62,22 @@ class ProfileFragment : Fragment() {
         binding.tb.navigationIcon?.setTint(resources.getColor(R.color.colorOnPrimary_900))
 
         activity?.getPreferences(Context.MODE_PRIVATE).let {
-            val name = it?.getString("name", null)
-            val username = "@${it?.getString("username", null)}"
+            it?.let {
 
-            binding.name.setText(name)
-            binding.username.text = username
+                val token = it.getString("token", null)
+                val name = it.getString("name", null)
+                val username = "@${it.getString("username", null)}"
+                val imgUrl = it.getString("img_url", null)
+
+                binding.name.setText(name)
+                binding.username.text = username
+                Glide.with(this).load(imgUrl)
+                    .circleCrop()
+                    .apply(RequestOptions.overrideOf(125, 125))
+                    .into(binding.img)
+
+                viewModel.token = token ?: ""
+            }
         }
 
         val menuEditItem = binding.tb.menu.findItem(R.id.edit)
@@ -73,6 +106,20 @@ class ProfileFragment : Fragment() {
                 InputMethodManager.HIDE_IMPLICIT_ONLY
             )
 
+//            binding.img.let { img ->
+//                img.setOnClickListener {
+//
+//                    val intent = Intent(Intent.ACTION_PICK).apply {
+//                        this.type = "image/*"
+//                        this.putExtra(Intent.EXTRA_MIME_TYPES, arrayOf("image/jpeg", "image/png"))
+//                    }
+//                    if (intent.resolveActivity(activity?.packageManager!!) != null) {
+//                        startActivityForResult(intent, 1)
+//                    }
+//                }
+//
+//            }
+
 
             true
         }
@@ -85,6 +132,10 @@ class ProfileFragment : Fragment() {
 
             imm.hideSoftInputFromWindow(binding.name.windowToken, 0)
 
+            viewModel.updateName(binding.name.text.toString())
+//            viewModel.updatePhoto()
+
+
             true
         }
 
@@ -92,4 +143,46 @@ class ProfileFragment : Fragment() {
         return binding.root
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == 1) {
+                viewModel.imgPath = data?.data?.path!!
+
+//                binding.img.setImageURI(img)
+//                val array = arrayOf(MediaStore.Images.Media.DATE_ADDED)
+//                val cursor =
+//                    activity?.contentResolver?.query(img ?: Uri.EMPTY, array, null, null, null)
+//                cursor?.moveToFirst()
+//                val columnIndex = cursor?.getColumnIndex(array[0])
+//                val imgString = cursor?.getString(columnIndex!!)
+//                cursor?.close()
+
+//                viewModel.imgPath = img?.path ?: ""
+//               val file = File(img?.lastPathSegment!!)
+//                Timber.i(file.absolutePath)
+//                Timber.i(img.lastPathSegment!!)
+//                Timber.i(URI(img!!).toString())
+
+            }
+        } else {
+            snackbar("Error occurred!")
+        }
+    }
+
+    override fun snackbar(value: String) {
+        Snackbar.make(binding.cool, value, Snackbar.LENGTH_LONG)
+            .setAnimationMode(Snackbar.ANIMATION_MODE_SLIDE).show()
+    }
+
+    override fun updateName(name: String) {
+        activity?.getPreferences(Context.MODE_PRIVATE)?.edit {
+            this.putString("name", name)
+        }
+    }
+}
+
+internal interface UpdateName{
+    fun updateName(name: String)
 }
---
+++ app/src/main/java/com/apps/chatychaty/viewModel/ProfileViewModel.kt
@@ -0,0 +1,60 @@
+package com.apps.chatychaty.viewModel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.apps.chatychaty.repo.UserRepository
+import com.apps.chatychaty.ui.UpdateName
+import kotlinx.coroutines.launch
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import retrofit2.HttpException
+import timber.log.Timber
+import java.io.File
+
+internal class ProfileViewModel(private val userRepository: UserRepository) : ViewModel() {
+
+    internal lateinit var updateName: UpdateName
+
+    internal lateinit var error: Error
+
+    var token = ""
+
+    var imgPath = ""
+
+    fun updatePhoto() {
+        viewModelScope.launch {
+            val file = File(imgPath)
+            Timber.i(file.absoluteFile.toString())
+            val body = file.asRequestBody("image/*".toMediaTypeOrNull())
+            val mp = MultipartBody.Part.createFormData("img", file.name, body)
+            userRepository.updatePhoto(token, mp)
+        }
+    }
+
+    fun updateName(name: String) {
+        viewModelScope.launch {
+            try {
+                userRepository.updateName(token, name)
+                updateName.updateName(name)
+            } catch (e: HttpException) {
+                error.snackbar(e.response().toString())
+            }
+        }
+    }
+
+}
+
+internal class ProfileViewModelFactory(private val userRepository: UserRepository) :
+    ViewModelProvider.Factory {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ProfileViewModel::class.java)) {
+            return ProfileViewModel(userRepository) as T
+        }
+        throw KotlinNullPointerException("Unknown ViewModel Class")
+    }
+
+}
