3313a7e7b571365034b652664a93597eb786729f
alialbaali <alialbaali@outlook.com>
2020-03-27 13:36:53 +0300

    Fix a bug where status bar icons' colors stay dark on Dark Mode

--- app/src/main/java/com/apps/chatychaty/MainActivity.kt
+++ app/src/main/java/com/apps/chatychaty/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.apps.chatychaty
 
-import android.content.Context
 import android.content.res.Configuration
 import android.os.Build
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import com.apps.chatychaty.database.AppDatabase
+import com.apps.chatychaty.network.DAOs
 
 const val DURATION = 500L
+var token: String? = null
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,32 +18,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val mode = resources.configuration.uiMode.and(Configuration.UI_MODE_NIGHT_MASK)
+        when (resources.configuration.uiMode.and(Configuration.UI_MODE_NIGHT_MASK)) {
+            Configuration.UI_MODE_NIGHT_YES ->
+                window.decorView.systemUiVisibility = 0
 
-////        window.statusBarColor = resources.getColor(R.color.colorOnPrimary_900)
-//
-//        when (mode) {
-//            Configuration.UI_MODE_NIGHT_YES -> {
-//                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-////                        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-//                }
-//            }
-//
-//            Configuration.UI_MODE_NIGHT_NO -> {
-////                window.decorView.systemUiVisibility = 0
-//            }
-//        }
-
-        getPreferences(Context.MODE_PRIVATE).let {
-            val token = it.getString("token", null)
-
-            if (token != null) {
-                this.findNavController(R.id.nav_host_fragment_container).let { nav ->
-                    nav.popBackStack()
-                    nav.navigate(R.id.listFragment)
+            Configuration.UI_MODE_NIGHT_NO
+            -> {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
                 }
             }
+        }
 
+        DAOs.chatDao = AppDatabase.getInstance(this).chatDao
+
+        DAOs.messageDao = AppDatabase.getInstance(this).messageDao
+
+        token = this.getPref("token")
+
+        if (token != null) {
+            this.findNavController(R.id.nav_host_fragment_container).let { nav ->
+                nav.popBackStack()
+                nav.navigate(R.id.listFragment)
+            }
         }
     }
 }
