d2076531052149fbc42b97af1cd1b072339ecfaf
alialbaali <alialbaali@outlook.com>
2020-03-27 16:37:56 +0300

    Replace getPreferences with setPref & getPref extension functions, Add Snackbar extenstion function

---
+++ app/src/main/java/com/apps/chatychaty/Utils.kt
@@ -0,0 +1,22 @@
+package com.apps.chatychaty
+
+import android.app.Activity
+import android.content.Context
+import android.view.View
+import androidx.core.content.edit
+import com.google.android.material.snackbar.Snackbar
+
+fun View.snackbar(value: String) {
+    Snackbar.make(this, value, Snackbar.LENGTH_LONG).setAnimationMode(Snackbar.ANIMATION_MODE_SLIDE)
+        .show()
+}
+
+fun Activity.setPref(key: String, value: String) {
+    this.getPreferences(Context.MODE_PRIVATE).edit {
+        this.putString(key, value)
+    }
+}
+
+fun Activity.getPref(key: String): String? {
+    return this.getPreferences(Context.MODE_PRIVATE).getString(key, null)
+}
