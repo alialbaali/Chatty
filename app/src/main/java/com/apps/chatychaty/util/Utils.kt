e9a7b277941a1a395e72d6da8619841bed3216db
alialbaali <alialbaali@outlook.com>
2020-04-07 11:37:05 +0300

    Refactor app icons

--- app/src/main/java/com/apps/chatychaty/util/Utils.kt
+++ app/src/main/java/com/apps/chatychaty/util/Utils.kt
@@ -4,7 +4,11 @@ import android.app.Activity
 import android.content.Context
 import android.view.View
 import androidx.core.content.edit
+import com.apps.chatychaty.viewModel.Error
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.CoroutineExceptionHandler
+import retrofit2.HttpException
+import java.net.UnknownHostException
 
 fun View.snackbar(value: String) {
     Snackbar.make(this, value, Snackbar.LENGTH_LONG).setAnimationMode(Snackbar.ANIMATION_MODE_SLIDE)
@@ -22,4 +26,20 @@ fun Activity.setPref(key: String, value: String?) {
 
 fun Activity.getPref(key: String): String? {
     return this.getPreferences(Context.MODE_PRIVATE).getString(key, null)
+}
+
+internal object ExceptionHandler {
+    lateinit var error: Error
+
+    val handler = CoroutineExceptionHandler { _, throwable ->
+        if (throwable is UnknownHostException) {
+        error.snackbar("Please connect to the internet")
+        }
+        if (throwable is HttpException) {
+            error.snackbar(throwable.message().toString())
+//            val errorResponse =
+//                ErrorConverter.converter.convert(throwable.response()?.errorBody()!!)
+//            error.snackbar()
+        }
+    }
 }
