9fb9a9a3e5325a64ad8a0238283d04e527e2c265
alialbaali <alialbaali@protonmail.ch>
2020-06-04 12:57:06 +0300

    Design Change Password Dialog Fragment

---
+++ app/src/main/java/com/chatychaty/app/profile/PasswordDialogFragment.kt
@@ -0,0 +1,56 @@
+package com.chatychaty.app.profile
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import com.chatychaty.app.BaseBottomSheetDialogFragment
+import com.chatychaty.app.databinding.FragmentDialogPasswordBinding
+import com.chatychaty.app.util.toast
+import org.koin.android.viewmodel.ext.android.viewModel
+
+class PasswordDialogFragment : BaseBottomSheetDialogFragment() {
+
+    private val binding by lazy {
+        FragmentDialogPasswordBinding.inflate(layoutInflater).also {
+            it.lifecycleOwner = this
+            it.viewModel = viewModel
+        }
+    }
+
+    private val viewModel by viewModel<ProfileViewModel>()
+
+    private val imm by lazy {
+        requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        binding.etCurrentPassword.requestFocus()
+        imm.showSoftInput(binding.etCurrentPassword, InputMethodManager.SHOW_IMPLICIT)
+
+
+        binding.btnConfirm.setOnClickListener {
+
+            val newPassword = binding.etNewPassword.text.toString()
+            val newPasswordConfirm = binding.etNewPasswordConfirm.text.toString()
+
+            if (newPassword != newPasswordConfirm) {
+
+                binding.tilNewPassword.error = "Passwords don't match"
+                binding.tilNewPasswordConfirm.error = "Passwords don't match"
+
+            } else {
+                imm.hideSoftInputFromWindow(binding.etCurrentPassword.windowToken, InputMethodManager.HIDE_IMPLICIT_ONLY)
+                dismiss()
+
+                it.toast("TODO")
+
+            }
+        }
+
+        return binding.root
+    }
+}
