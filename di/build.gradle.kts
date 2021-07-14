f603e9529d853dde58bc6dce28eea4576cebad34
Ali Albaali <ali@albaali.com>
2021-07-14 22:40:54 +0300

    Update DI module

--- di/build.gradle.kts
+++ di/build.gradle.kts
@@ -1,25 +1,21 @@
-import com.chatychaty.buildSrc.*
 plugins {
-    val plugins = com.chatychaty.buildSrc.Plugins
-    id(plugins.ANDROID_LIBRARY)
-    kotlin(plugins.KOTLIN_ANDROID)
-    id(plugins.KOTLIN_ANDROID_EXTENSIONS)
+    id(Plugins.ANDROID_LIBRARY)
+    kotlin(Plugins.KOTLIN_ANDROID)
 }
 
 android {
-    compileSdkVersion(App.COMPILE_SDK)
-    buildToolsVersion(App.BUILD_TOOLS)
+    buildToolsVersion = App.BUILD_TOOLS
+    compileSdk = App.COMPILE_SDK
     defaultConfig {
-        minSdkVersion(App.MIN_SDK)
-        targetSdkVersion(App.TARGET_SDK)
-        versionCode = App.APP_VERSION_CODE
-        versionName = App.APP_VERSION_NAME
-
+        minSdk = App.MIN_SDK
+        targetSdk = App.TARGET_SDK
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
+
     buildTypes {
         getByName("release") {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            signingConfig = signingConfigs.getByName("debug")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -27,8 +23,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 
     kotlinOptions {
@@ -41,4 +37,6 @@ dependencies {
     implementation(project(Modules.DATA))
     implementation(project(Modules.LOCAL))
     implementation(project(Modules.REMOTE))
+    implementation(Libraries.ROOM)
+    implementation(Libraries.KOIN)
 }
--- di/src/main/java/com/chatychaty/di/Modules.kt
+++ di/src/main/java/com/chatychaty/di/Modules.kt
@@ -2,21 +2,19 @@ package com.chatychaty.di
 
 import android.content.Context
 import android.content.SharedPreferences
+import androidx.datastore.preferences.preferencesDataStore
 import com.chatychaty.data.repository.ChatRepositoryImpl
 import com.chatychaty.data.repository.MessageRepositoryImpl
 import com.chatychaty.data.repository.UserRepositoryImpl
-import com.chatychaty.domain.interactor.chat.*
-import com.chatychaty.domain.interactor.message.*
-import com.chatychaty.domain.interactor.user.*
+import com.chatychaty.domain.repository.ChatRepository
+import com.chatychaty.domain.repository.MessageRepository
 import com.chatychaty.domain.repository.UserRepository
-import com.chatychaty.domain.usecase.chat.CheckUpdates
-import com.chatychaty.local.AppDatabase
-import com.chatychaty.local.ChatDao
-import com.chatychaty.local.MessageDao
-import com.chatychaty.local.UserDao
-import com.chatychaty.remote.ChatClient
-import com.chatychaty.remote.MessageClient
-import com.chatychaty.remote.UserClient
+import com.chatychaty.local.*
+import com.chatychaty.remote.chat.RemoteChatDataSource
+import com.chatychaty.remote.message.RemoteMessageDataSource
+import com.chatychaty.remote.message.SignalRHub
+import com.chatychaty.remote.message.SignalRHubImpl
+import com.chatychaty.remote.user.RemoteUserDataSource
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import okhttp3.OkHttpClient
@@ -37,13 +35,13 @@ private val TIMEUNIT = TimeUnit.SECONDS
 
 val remoteModule = module {
 
-    single { HttpLoggingInterceptor().also { it.level = HttpLoggingInterceptor.Level.BODY } }
+    single<HttpLoggingInterceptor> { HttpLoggingInterceptor().also { it.level = HttpLoggingInterceptor.Level.BODY } }
 
-    single { Moshi.Builder().add(KotlinJsonAdapterFactory()).build() as Moshi }
+    single<Moshi> { Moshi.Builder().add(KotlinJsonAdapterFactory()).build() }
 
-    single {
+    single<OkHttpClient> {
         OkHttpClient.Builder()
-            .addInterceptor(get() as HttpLoggingInterceptor)
+            .addInterceptor(get<HttpLoggingInterceptor>())
 //            .addNetworkInterceptor(object : NetworkConnectionInterceptor() {
 //
 //                lateinit var listener: NetworkConnectionListener
@@ -64,102 +62,58 @@ val remoteModule = module {
             .build()
     }
 
-    single { Retrofit.Builder().addConverterFactory(MoshiConverterFactory.create(get())).baseUrl(BASE_URL).client(get()).build() as Retrofit }
+    single<Retrofit> {
+        val moshi = get<Moshi>()
+        val moshiConverterFactory = MoshiConverterFactory.create(moshi)
 
-    single { (get() as Retrofit).create(MessageClient::class.java) }
+        Retrofit
+            .Builder()
+            .addConverterFactory(moshiConverterFactory)
+            .baseUrl(BASE_URL)
+            .client(get<OkHttpClient>())
+            .build()
+    }
 
-    single { (get() as Retrofit).create(ChatClient::class.java) }
+    single<RemoteMessageDataSource> { (get() as Retrofit).create(RemoteMessageDataSource::class.java) }
 
-    single { (get() as Retrofit).create(UserClient::class.java) }
+    single<RemoteChatDataSource> { (get() as Retrofit).create(RemoteChatDataSource::class.java) }
+
+    single<RemoteUserDataSource> { (get() as Retrofit).create(RemoteUserDataSource::class.java) }
+
+    single<SignalRHub> { SignalRHubImpl() }
 }
 
 val repositoryModule = module {
-    single {
+    single<MessageRepository> {
         MessageRepositoryImpl(
-            get() as MessageClient,
-            get() as MessageDao,
-            get() as UserDao
+            get<RemoteMessageDataSource>(),
+            get<MessageDao>(),
+            get<UserDao>(),
+            signalRHub = get<SignalRHub>(),
         )
     }
 
-    single {
+    single<ChatRepository> {
         ChatRepositoryImpl(
-            get() as ChatClient,
-            get() as ChatDao,
-            get() as UserDao
+            get<RemoteChatDataSource>(),
+            get<ChatDao>(),
+            get<UserDao>(),
+            signalRHub = get<SignalRHub>(),
         )
     }
 
-    single { UserRepositoryImpl(get() as UserDao, get() as UserClient) }
+    single<UserRepository> { UserRepositoryImpl(get<UserDao>(), get<RemoteUserDataSource>()) }
 }
 
 val localModule = module {
-    single { AppDatabase.getInstance(androidContext()).chatDao }
+    single<ChatDao> { AppDatabase.getInstance(androidContext()).chatDao }
 
-    single { AppDatabase.getInstance(androidContext()).messageDao }
+    single<MessageDao> { AppDatabase.getInstance(androidContext()).messageDao }
 
-    single { androidContext().getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE) as SharedPreferences }
+    single<SharedPreferences> { androidContext().getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE) }
 
-    single { UserDao(get()) }
-}
-
-val chatUseCaseModule = module {
-
-    single { ChatUseCases(get(), get(), get(), get(), get(), get()) }
-
-    single { CreateChat(get() as ChatRepositoryImpl) }
-
-    single { GetChats(get() as ChatRepositoryImpl) }
-
-    single { CheckUpdates(get() as ChatRepositoryImpl, get() as MessageRepositoryImpl) }
-
-    single { GetMessages(get() as MessageRepositoryImpl) }
-
-    single { GetRemoteChats(get() as ChatRepositoryImpl) }
-
-    single { GetChatById(get() as ChatRepositoryImpl) }
-}
-
-val messageUseCaseModule = module {
-
-    single { MessageUseCase(get(), get(), get()) }
-
-    single { CreateMessage(get() as MessageRepositoryImpl) }
-
-    single { GetLastMessage(get() as MessageRepositoryImpl) }
-
-    single { GetRemoteMessages(get() as MessageRepositoryImpl) }
+    single<UserDao> { UserDaoImpl(androidContext().dataStore) }
 
 }
 
-val userUseCaseModule = module {
-
-    single { UserUseCase(get(), get(), get(), get(), get(), get(), get(), get(), get(), get(), get()) }
-
-    single { SignUp(get() as UserRepositoryImpl) }
-
-    single { SignIn(get() as UserRepositoryImpl) }
-
-    single { UpdatePhoto(get() as UserRepositoryImpl) }
-
-    single { UpdateName(get() as UserRepositoryImpl) }
-
-    single { GetTheme(get() as UserRepositoryImpl) }
-
-    single { SetTheme(get() as UserRepositoryImpl) }
-
-    single {
-        SignOut(
-            get() as UserRepositoryImpl,
-            get() as ChatRepositoryImpl,
-            get() as MessageRepositoryImpl
-        )
-    }
-    single { GetChatUser(get() as ChatRepositoryImpl) }
-
-    single { GetUser(get() as UserRepositoryImpl) }
-
-    single { SubmitFeedback(get() as UserRepositoryImpl)  }
-
-    single { DeleteAccount(get() as UserRepositoryImpl, get() as ChatRepositoryImpl, get() as MessageRepositoryImpl)  }
-}
+private val Context.dataStore by preferencesDataStore("ChatyChaty")
