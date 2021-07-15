390eba6aabc5f746353bef38b83ee0f8f2820ac9
Ali Albaali <ali@albaali.com>
2021-07-15 23:46:29 +0300

    Update chat dialogs

---
+++ app/src/main/java/com/chatychaty/app/chat/ChatItemDialogFragment.kt
@@ -0,0 +1,183 @@
+package com.chatychaty.app.chat
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.chatychaty.app.R
+import com.chatychaty.app.databinding.FragmentDialogChatBinding
+import com.chatychaty.app.message.MessageListViewModel
+import com.chatychaty.app.util.BaseBottomSheetDialogFragment
+import com.chatychaty.app.util.UiState
+import com.chatychaty.app.util.snackbar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import org.koin.core.parameter.parametersOf
+
+class ChatItemDialogFragment : BaseBottomSheetDialogFragment() {
+
+    private lateinit var binding: FragmentDialogChatBinding
+
+    private val args by navArgs<ChatItemDialogFragmentArgs>()
+
+    private val viewModel by viewModel<MessageListViewModel> { parametersOf(args.chatId) }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+
+        binding = FragmentDialogChatBinding.inflate(inflater, container, false).also {
+            it.lifecycleOwner = this
+        }
+
+        collectState()
+        setupListeners()
+
+        return binding.root
+    }
+
+
+    @SuppressLint("SetTextI18n")
+    private fun collectState() {
+
+        viewModel.chat
+            .onEach { state ->
+                when (state) {
+                    is UiState.Failure -> {
+
+                    }
+                    is UiState.Loading -> {
+
+                    }
+                    is UiState.Success -> {
+                        val chat = state.value
+                        binding.tvViewProfile.text = "View ${chat.name}'s profile"
+
+                        val archiveDrawable = ResourcesCompat.getDrawable(
+                            resources,
+                            if (chat.isArchived) R.drawable.ic_round_unarchive_24 else R.drawable.ic_round_archive_24,
+                            null,
+                        )
+                        binding.tvArchiveChat.text = getString(if (chat.isArchived) R.string.unarchive_chat else R.string.archive_chat)
+                        binding.tvArchiveChat.setCompoundDrawablesWithIntrinsicBounds(archiveDrawable, null, null, null)
+
+                        val pinDrawable = ResourcesCompat.getDrawable(
+                            resources,
+                            if (chat.isPinned) R.drawable.ic_round_pin_off_24 else R.drawable.ic_round_pin_24,
+                            null,
+                        )
+                        binding.tvPin.setCompoundDrawablesWithIntrinsicBounds(pinDrawable, null, null, null)
+                        binding.tvPin.text = getString(if (chat.isPinned) R.string.unpin else R.string.pin)
+
+                        val muteDrawable = ResourcesCompat.getDrawable(
+                            resources,
+                            if (chat.isMuted) R.drawable.ic_round_volume_up_24 else R.drawable.ic_round_volume_off_24,
+                            null,
+                        )
+                        binding.tvMute.setCompoundDrawablesWithIntrinsicBounds(muteDrawable, null, null, null)
+                        binding.tvMute.text = getString(if (chat.isMuted) R.string.unmute else R.string.mute)
+                    }
+                    UiState.Empty -> {
+
+                    }
+                }
+            }
+            .launchIn(lifecycleScope)
+    }
+
+    private fun setupListeners() {
+        val parentFragmentFab = requireParentFragment()
+            .requireView()
+            .findViewById<FloatingActionButton>(R.id.fab)
+
+
+        binding.tvArchiveChat.setOnClickListener {
+            dismiss()
+            if ((viewModel.chat.value as UiState.Success).value.isArchived) {
+                viewModel.unarchiveChat()
+                requireParentFragment()
+                    .requireView()
+                    .snackbar(getString(R.string.chat_is_unarchived))
+            } else {
+                viewModel.archiveChat()
+
+                requireParentFragment()
+                    .requireView()
+                    .snackbar(
+                        getString(R.string.chat_is_archived),
+                        anchorView = parentFragmentFab,
+                    )
+            }
+        }
+
+        binding.tvChangeColor.setOnClickListener {
+            dismiss()
+            requireParentFragment()
+                .requireView()
+                .snackbar("TODO", anchorView = parentFragmentFab)
+        }
+
+        binding.tvDeleteChat.setOnClickListener {
+            dismiss()
+            requireParentFragment()
+                .requireView()
+                .snackbar("TODO", anchorView = parentFragmentFab)
+        }
+
+        binding.tvPin.setOnClickListener {
+            dismiss()
+            if ((viewModel.chat.value as UiState.Success).value.isPinned) {
+                viewModel.unpinChat()
+                requireParentFragment()
+                    .requireView()
+                    .snackbar(
+                        getString(R.string.chat_is_unpinned),
+                        anchorView = parentFragmentFab
+                    )
+            } else {
+                viewModel.pinChat()
+
+                requireParentFragment()
+                    .requireView()
+                    .snackbar(
+                        getString(R.string.chat_is_pinned),
+                        anchorView = parentFragmentFab,
+                    )
+            }
+        }
+
+        binding.tvViewProfile.setOnClickListener {
+            dismiss()
+            findNavController().navigate(ChatItemDialogFragmentDirections.actionChatDialogFragmentToProfileFragment(args.chatId))
+        }
+
+        binding.tvMute.setOnClickListener {
+            dismiss()
+            if ((viewModel.chat.value as UiState.Success).value.isMuted) {
+                viewModel.unmuteChat()
+                requireParentFragment()
+                    .requireView()
+                    .snackbar(
+                        getString(R.string.chat_is_unmuted),
+                        anchorView = parentFragmentFab
+                    )
+            } else {
+                viewModel.muteChat()
+
+                requireParentFragment()
+                    .requireView()
+                    .snackbar(
+                        getString(R.string.chat_is_muted),
+                        anchorView = parentFragmentFab,
+                    )
+            }
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/chatychaty/app/chat/ChatListDialogFragment.kt
@@ -0,0 +1,53 @@
+package com.chatychaty.app.chat
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.chatychaty.app.R
+import com.chatychaty.app.databinding.FragmentDialogListChatBinding
+import com.chatychaty.app.util.BaseBottomSheetDialogFragment
+
+class ChatListDialogFragment : BaseBottomSheetDialogFragment() {
+
+    private lateinit var binding: FragmentDialogListChatBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+
+        binding = FragmentDialogListChatBinding.inflate(inflater, container, false).also {
+            it.lifecycleOwner = this
+        }
+
+        binding.tvArchivedChats.setOnClickListener {
+            dismiss()
+            findNavController().navigate(ChatListDialogFragmentDirections.actionListDialogFragmentToChatListArchiveFragment())
+        }
+
+        binding.tvInviteOthers.setOnClickListener {
+            dismiss()
+            val intent = Intent(Intent.ACTION_SEND).apply {
+                type = "text/plain"
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.invite_text))
+            }
+
+            val chooser = Intent.createChooser(intent, getString(R.string.invite_others))
+
+            startActivity(chooser)
+        }
+
+        binding.tvSettings.setOnClickListener {
+            dismiss()
+            findNavController().navigate(ChatListDialogFragmentDirections.actionListDialogFragmentToUserFragment())
+        }
+
+        binding.tvTheme.setOnClickListener {
+            dismiss()
+            findNavController().navigate(ChatListDialogFragmentDirections.actionListDialogFragmentToThemeDialogFragment())
+        }
+
+        return binding.root
+    }
+
+}
