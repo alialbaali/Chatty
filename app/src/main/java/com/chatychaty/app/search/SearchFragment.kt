ee3497c9487c348e64f2f40b5068648c0e4ae72c
Ali Albaali <ali@albaali.com>
2021-07-15 23:48:06 +0300

    Update search fragment

--- app/src/main/java/com/chatychaty/app/search/SearchFragment.kt
+++ app/src/main/java/com/chatychaty/app/search/SearchFragment.kt
@@ -8,39 +8,52 @@ import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
-import com.chatychaty.app.databinding.FragmentSearchBinding
 import com.chatychaty.app.R
-import org.koin.android.viewmodel.ext.android.viewModel
+import com.chatychaty.app.databinding.FragmentSearchBinding
+import com.chatychaty.app.util.ProgressDialogFragment
+import com.chatychaty.app.util.UiState
+import com.chatychaty.app.util.snackbar
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 /**
  * A simple [Fragment] subclass.
  */
 class SearchFragment : Fragment() {
 
-    private val binding by lazy {
-        FragmentSearchBinding.inflate(layoutInflater).also {
-            it.lifecycleOwner = this
-            it.viewModel = viewModel
-        }
-    }
+    private lateinit var binding: FragmentSearchBinding
 
     private val viewModel by viewModel<SearchViewModel>()
+
+    private lateinit var progressDialogFragment: ProgressDialogFragment
 
     private val imm by lazy {
         requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+
+        binding = FragmentSearchBinding.inflate(inflater, container, false).also {
+            it.lifecycleOwner = this
+            it.viewModel = viewModel
+        }
 
         binding.tb.navigationIcon?.setTint(resources.getColor(R.color.colorPrimary))
 
+        setListeners()
+        collectState()
+
+        return binding.root
+    }
+
+    private fun setListeners() {
+
         binding.tb.setNavigationOnClickListener {
             imm.hideSoftInputFromWindow(binding.et.windowToken, 0)
-            this.findNavController().navigateUp()
+            findNavController().navigateUp()
         }
 
         binding.et.requestFocus()
@@ -49,13 +62,39 @@ class SearchFragment : Fragment() {
         binding.et.setOnEditorActionListener { v, actionId, event ->
             if (actionId == EditorInfo.IME_ACTION_DONE) {
 
+                viewModel.createChat()
+
                 imm.hideSoftInputFromWindow(v.windowToken, 0)
-
-                this.findNavController().navigateUp()
+                true
+            } else {
+                false
             }
-            true
         }
+    }
 
-        return binding.root
+    private fun collectState() {
+        viewModel.state
+            .onEach {
+                when (it) {
+                    is UiState.Empty -> {
+
+                    }
+                    is UiState.Failure -> {
+                        progressDialogFragment.dismiss()
+                        binding.root.snackbar(it.exception.message.toString())
+                    }
+                    is UiState.Loading -> {
+                        progressDialogFragment = ProgressDialogFragment()
+                            .also { it.show(parentFragmentManager, null) }
+                    }
+                    is UiState.Success -> {
+                        progressDialogFragment.dismiss()
+                        findNavController().navigateUp()
+                        binding.root.snackbar(it.value)
+                    }
+                }
+            }
+            .launchIn(lifecycleScope)
+
     }
 }
--- app/src/main/java/com/chatychaty/app/search/SearchViewModel.kt
+++ app/src/main/java/com/chatychaty/app/search/SearchViewModel.kt
@@ -1,11 +1,35 @@
 package com.chatychaty.app.search
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.chatychaty.app.util.UiState
+import com.chatychaty.domain.repository.ChatRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
-class SearchViewModel : ViewModel() {
+class SearchViewModel(private val chatRepository: ChatRepository) : ViewModel() {
 
-    val username = MutableLiveData<String>().also {
-        it.value = ""
+    val username = MutableStateFlow("")
+
+    private val mutableState = MutableStateFlow<UiState<String>>(UiState.Empty)
+    val state get() = mutableState.asStateFlow()
+
+    fun createChat() {
+        viewModelScope.launch {
+
+            mutableState.value = UiState.Loading
+
+            chatRepository.createChat(username.value)
+                .fold(
+                    onSuccess = {
+                        mutableState.value = UiState.Success("User is found!")
+                    },
+                    onFailure = {
+                        mutableState.value = UiState.Failure(it)
+                    }
+                )
+        }
     }
+
 }
